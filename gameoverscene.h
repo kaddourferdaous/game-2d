f9c7c58663e3444a6ea88c53fea1d34c70cab732
kaddourferdaous <121985701+kaddourferdaous@users.noreply.github.com>
2023-01-08 00:07:23 +0100

    Add files via upload

---
+++ gameoverscene.h
@@ -0,0 +1,30 @@
+
+
+#ifndef __PLAY_SCENE_H__
+#define __PLAY_SCENE_H__
+
+#include "cocos2d.h"
+
+class PlayScene : public cocos2d::Layer
+{
+public:
+
+    static cocos2d::Scene* createScene();
+
+    virtual bool init();
+
+
+    void menuCloseCallback(cocos2d::Ref* pSender);
+
+
+    CREATE_FUNC(PlayScene);
+
+    cocos2d::Sprite* mySprite;
+
+    void GoTopausescene(Ref* pSender);
+    void GoTogameoverscene(Ref* pSender);
+
+
+};
+
+#endif
